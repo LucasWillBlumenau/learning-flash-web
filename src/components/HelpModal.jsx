299710e529ec23778c9a18074d86d24ab698cb9d
LucasWillBlumenau <willlucas07@proton.me>
2023-11-02 19:23:06 -0300

    creating tips menu

--- src/components/HelpModal.jsx
+++ src/components/HelpModal.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './styles/HelpModal.css'
 import { FaArrowLeft } from 'react-icons/fa'
 
@@ -9,31 +10,57 @@ const HelpTip = ({ tipTitle, tipDescription }) => (
     </div>
 )
 
-const HelpTipsContainer = ({ children, setShowHelpMenu }) => (
-    <div className="modal">
-        <div className="popup helpMenu">
-            <FaArrowLeft className="closeMenuIcon" onClick={() => setShowHelpMenu(false)} />
-            {children}
-        </div>
-    </div>
-)
+const HelpTipsContainer = ({ children, setShowHelpMenu }) => {
+    
+    const [currentItem, setCurrentItem] = useState(0)
 
-export default ({ setShowHelpMenu }) => {
     return (
-        <HelpTipsContainer setShowHelpMenu={setShowHelpMenu}>
-            <HelpTip 
-                tipTitle="Pratique inglês utilizando decks e flashcards"
-                tipDescription="Os decks são os containers para os flashcards. Os flashcards são cartões que possuem frente e verso. Neles você pode adicionar frases em inglês e sua tradução para testar seus níveis de conhecimento e se aperfeiçoar."
-            />
-            <HelpTip 
-                tipTitle="Adicione seus decks e flashcards no menu de decks"
-                tipDescription="Você pode utilizar o menu decks para adicionar seus de decks e flashcards."
-            />
-            <HelpTip 
-                tipTitle="Adicione seus flashcards lendo os resumos"
-                tipDescription="Ao clicar em uma das frases enquanto você lê um resumo, um popup com a frase em que você clicou e sua tradução será exibido para que você a adicione em um de seus decks."
-            />
-        </HelpTipsContainer>
+        <div className="modal">
+            <div className="popup helpMenu">
+                <FaArrowLeft className="closeMenuIcon" onClick={() => setShowHelpMenu(false)} />
+                <div className="tipsSlider" style={{transform: `translateX(${currentItem * -100}%)`}}>
+                    {children}
+                </div>
+                <div className="tipsOptions">
+                    {children.map((_, index) => (
+                        <button
+                            className={index !== currentItem? 'tipOption': 'tipOption selected'}
+                            onClick={() => setCurrentItem(index)}>
+                        </button>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
+export default ({ setShowHelpMenu }) => {
+
+    const tips = [
+        {
+            title: "Pratique inglês utilizando decks e flashcards",
+            description: "Os decks são os containers para os flashcards. Os flashcards são cartões que possuem frente e verso. Neles você pode adicionar frases em inglês e sua tradução para testar seus níveis de conhecimento e se aperfeiçoar."
+        },
+        {
+            title: "Adicione seus decks e flashcards no menu de decks",
+            description: "Você pode utilizar o menu decks para adicionar seus de decks e flashcards.",
+        },
+        {
+            title: "Adicione seus flashcards lendo os resumos",
+            description: "Ao clicar em uma das frases enquanto você lê um resumo, um popup com a frase em que você clicou e sua tradução será exibido para que você a adicione em um de seus decks.",
+        }
+
+    ]
+
+    return (
+        <HelpTipsContainer setShowHelpMenu={setShowHelpMenu}>
+            {tips.map(tip => (
+                <HelpTip 
+                    tipTitle={tip.title}
+                    tipDescription={tip.description}
+                />
+            ))}
+
+        </HelpTipsContainer>
+    )
+}
